a3b22fe35bfaa9173b853f4d2a49688f24199cdf
Nicola Galeotti <niccio2802@outlook.it>
2020-08-11 11:24:02 +0200

    Add project files.

---
+++ TemperatureLibrary/Temperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TemperatureLibrary
+{
+    public class Temperature
+    {
+        public static bool IsSottoZero(int n)
+        {
+            bool resp;
+            if (n < 0)
+                resp = true;
+            else resp = false;
+            return resp;
+        }
+    }
+}
